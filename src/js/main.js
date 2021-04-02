da3248223dac61c791b4526bef7ea896487d355d
ctalloc <cecilia.tallo@gmail.com>
2021-04-02 12:56:58 +0200

    Add ComfyCounter and Pet Head Button functionality

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-console.log('>> Ready :)');
+let comfyCounterElement = 0;
+
+const headButtonElement = document.querySelector('.jsHeadBtn');
+const comfyNumberTxt = document.querySelector(".js-comfyNmbr");
+
+function handleHeadButton() {
+  comfyCounterElement += 2;
+  console.log(comfyCounterElement);
+  handleNmbrTxt();
+}
+
+function handleNmbrTxt() {
+  let html = comfyCounterElement;
+  comfyNumberTxt.innerHTML = html;
+}
+
+headButtonElement.addEventListener('click', handleHeadButton);
