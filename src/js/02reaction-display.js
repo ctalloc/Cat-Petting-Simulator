a24344db4cc4c1e1336ee2aab24b30cadcad2850
ctalloc <cecilia.tallo@gmail.com>
2021-04-18 21:29:59 +0200

    :broom: clean redundant and repeated functions

--- src/js/02reaction-display.js
+++ src/js/02reaction-display.js
@@ -4,42 +4,46 @@ function goodReaction() {
   catReactDisplay.classList.add('reactNow')
   let src = './assets/images/cat-reactions/happycat.jpg';
   catReactDisplay.src = src;
-  setTimeout(stopAnimation, 1000)
+  clearDisplay()
 }
 
 function loveReaction() {
   catReactDisplay.classList.add('reactNow')
   let src = './assets/images/cat-reactions/lovecat.jpg';
   catReactDisplay.src = src;
-  setTimeout(stopAnimation, 1000)
+  clearDisplay()
 }
 
 function sadReaction() {
   catReactDisplay.classList.add('reactNow')
   let src = './assets/images/cat-reactions/sadcat.jpg';
   catReactDisplay.src = src;
-  setTimeout(stopAnimation, 1000)
+  clearDisplay()
 }
 
 function angryReaction() {
   catReactDisplay.classList.add('reactNow')
   let src = './assets/images/cat-reactions/angrycat.jpg';
   catReactDisplay.src = src;
-  setTimeout(stopAnimation, 1000)
+  clearDisplay()
 }
 
 function ottReaction() {
   catReactDisplay.classList.add('reactNow')
   let src = './assets/images/cat-reactions/butwhy.jpg';
   catReactDisplay.src = src;
-  setTimeout(stopAnimation, 1000)
+  clearDisplay()
 }
 
-function clearDisplay() {
+function clearImgSrc() {
   catReactDisplay.src = '';
 }
 
 function stopAnimation() {
   catReactDisplay.classList.remove('reactNow')
-  clearDisplay() 
+} 
+
+function clearDisplay() {
+  setTimeout(stopAnimation, 700)
+  setTimeout(clearImgSrc, 710)
 } 
