e2c1dcfdb161212295b472b3d6d7d456f6dab67e
ctalloc <cecilia.tallo@gmail.com>
2021-04-18 20:39:24 +0200

    :dizzy: Write more reaction animation functions.

--- src/js/02reaction-display.js
+++ src/js/02reaction-display.js
@@ -8,6 +8,30 @@ function goodReaction() {
   setTimeout(stopAnimation, 1000)
 }
 
+function loveReaction() {
+  catReactDisplay.classList.add('reactNow')
+  let src = './assets/images/cat-reactions/lovecat.jpg';
+  catReactDisplay.src = src;
+  setTimeout(clearDisplay, 500);
+  setTimeout(stopAnimation, 1000)
+}
+
+function sadReaction() {
+  catReactDisplay.classList.add('reactNow')
+  let src = './assets/images/cat-reactions/sadcat.jpg';
+  catReactDisplay.src = src;
+  setTimeout(clearDisplay, 500);
+  setTimeout(stopAnimation, 1000)
+}
+
+function angryReaction() {
+  catReactDisplay.classList.add('reactNow')
+  let src = './assets/images/cat-reactions/angrycat.jpg';
+  catReactDisplay.src = src;
+  setTimeout(clearDisplay, 500);
+  setTimeout(stopAnimation, 1000)
+}
+
 function clearDisplay() {
   catReactDisplay.src = '';
 }
